3d11fb406522eaf0380b81e1b5246912b7610c12
andrei.bekh <andrei.bekh@leverx.com>
2021-12-28 14:13:05 +0300

    Configured hibernate

---
+++ src/main/java/config/HibernateSessionFactoryUtil.java
@@ -0,0 +1,32 @@
+package config;
+
+import model.AnimalEntity;
+import model.NoteEntity;
+import model.PersonEntity;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateSessionFactoryUtil {
+    private static SessionFactory sessionFactory;
+
+    private HibernateSessionFactoryUtil() {
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration().configure();
+                configuration.addAnnotatedClass(AnimalEntity.class);
+                configuration.addAnnotatedClass(PersonEntity.class);
+                configuration.addAnnotatedClass(NoteEntity.class);
+                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+                sessionFactory = configuration.buildSessionFactory(builder.build());
+
+            } catch (Exception e) {
+                System.out.println("Exception!" + e);
+            }
+        }
+        return sessionFactory;
+    }
+}
